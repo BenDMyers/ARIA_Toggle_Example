654567324bdf24669b99ea1b6a46508d2e6d96d8
Ben Myers <ben.myers@okstate.edu>
2019-11-22 19:35:44 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ARIA Toggle Example
+
+## What is this?
+
+This is a template to follow for [my blogpost about ARIA](https://blog.benmyers.dev/aria/). In that blogpost, I introduce how ARIA can influence HTML elements' names, roles, states, and properties exposed to assistive technology.
+
+## How do I use this?
+
+First, pull the repository down locally:
+
+```bash
+git clone git remote add origin https://github.com/BenDMyers/ARIA_Toggle_Example.git
+```
+
+Open up `index.html` in your browser.
+
+Then follow along with [the blogpost](https://blog.benmyers.dev/aria/).
+
+## Show me the finished product!
+
+If you just want to see the finished product, see the [`finished`](https://github.com/BenDMyers/portfolio/tree/finished) branch.
+
+If you'd instead like to see the toggle recreated semantically, go to the [`semantic`](https://github.com/BenDMyers/portfolio/tree/semantic) branch.
---
+++ toggler.js
@@ -0,0 +1,29 @@
+const toggler = document.querySelector('.toggle-switch');
+const switchStatus = document.querySelector('.status');
+
+let switchIsActive = false;
+
+// Called whenever you click on the toggle
+function handleClick() {
+    // Causes page to alternate between light and dark mode
+    document.body.classList.toggle('dark-mode');
+
+    // Causes the toggle to change appearance
+    toggler.classList.toggle('active');
+
+    // Modifies status contents
+    switchIsActive = !switchIsActive;
+    switchStatus.innerHTML = switchIsActive ? 'on' : 'off';
+}
+
+// Adds keyboard events to the toggle
+toggler.addEventListener('keydown', function (event) {
+    if (event.key === ' ' || event.key === 'Enter') {
+        // Prevents unintentional form submissions, page scrolls, the like
+        event.preventDefault();
+
+        handleClick();
+    }
+});
+
+toggler.onclick = handleClick;
